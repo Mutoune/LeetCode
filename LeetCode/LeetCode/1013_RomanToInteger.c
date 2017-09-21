15a566cbf7109010e47d3a9247cd9b4f6f282c37
Minghan Xu <bluefoxxu@gmail.com>
2017-09-21 07:54:09 -0400

    1013_RomanToInteger

---
+++ LeetCode/LeetCode/1013_RomanToInteger.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+int romanToInt(char* s) {
+	int res = 0, temp = 0;
+	while (*s) {
+		switch (*s)
+		{
+			case 'I': res += 1; temp = 1; break;
+			case 'V': res += temp == 1 ? 3 : 5; break;
+			case 'X': res += temp == 1 ? 8 : 10; temp = 10; break;
+			case 'L': res += temp == 10 ? 30 : 50; break;
+			case 'C': res += temp == 10 ? 80 : 100; temp = 100; break;
+			case 'D': res += temp == 100 ? 300 : 500; break;
+			case 'M': res += temp == 100 ? 800 : 1000; break;
+		}
+		s++;
+	}
+	return res;
+}
+
+int main() {
+	printf("%d\n", romanToInt("IX"));
+	return 0;
+}
